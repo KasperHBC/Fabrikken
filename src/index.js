f8a9aca52ce13e27c24a8689dff53fb6e166100c
KasperHBC <kasc@mercantec.dk>
2024-12-10 13:43:24 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
-// src/index.js
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
-import { HashRouter as Router } from 'react-router-dom'; // Importer HashRouter
+import { BrowserRouter as Router } from 'react-router-dom'; // Brug BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/Fabrikken">
       <App />
     </Router>
   </React.StrictMode>
 );
 
-window.onerror = function (message, source, lineno, colno, error) {
-  console.error('Global error handler:', message, error);
-};
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
