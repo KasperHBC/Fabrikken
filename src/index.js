46987973ef2ee6c1e318925c6f97f59ef70cb4ce
KasperHBC <kasc@mercantec.dk>
2024-12-10 13:24:47 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
+import { HashRouter as Router } from 'react-router-dom'; // Importer HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
 window.onerror = function (message, source, lineno, colno, error) {
   console.error('Global error handler:', message, error);
 };
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
